92b68ef4eaef13da2354f0bc231ad1f845dd5ab0
LutzWave <Nicku.gonzalez@gmail.com>
2020-10-21 17:31:09 -0300

    Nicku
    Ahh

--- Form.js
+++ Form.js
@@ -1,31 +1,6 @@
-var Inputs = document.getElementsByClassName('Formulario_Input');
+Function validar {
+    var Nombre, correo, rut, dni;
+    Nombre = document.getElementById("Nombre").value;
+    Correo = document.getElementById("Correo").value;
 
-for (let index = 0; index < Inputs.length; index++) {
-    Inputs[index].addEventListener('keyup', function() {
-            if (this.value.length >= 1) {
-                this.nextElementSibling.ClassList.add('Fijar');
-            } else {
-                this.nextElementSibling.ClassList.remove('Fijar');
-            }
-
-        }
-
-
-    });
-
-
-var Formulario = document.getElementsByClassName('Formulario')
-boton = document.getElementById('btn');
-
-var ValidarNombre = function(e) {
-    if (Formulario.Formulario.Nombre == 0) {
-        alert("Complete su nombre");
-        e.preventDefault(e);
-    }
 }
-var Validar = function(e) {
-    ValidarNombre(e);
-
-};
-
-Formulario.addEventListener("Submit", Validar);
